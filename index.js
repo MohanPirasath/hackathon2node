7835a31271c340cd687ab03cc8b7945469879678
Mohanpirasath <mahendran.mohanmass2000@gmail.com>
2022-08-08 00:55:15 +0530

    logins

--- index.js
+++ index.js
@@ -44,6 +44,69 @@ app.post("/cam", async function(req,res){
     const set = await Client.db("B33WD").collection("Cart").insertOne(data)
     res.send(set)
 })
+app.post("/Adminlogin", async function(req,res){
+    const {username,password}=req.body
+    const Adminexist = await Client.db("FoodToken").collection("admins").findOne({username:username})
+    if (!Adminexist) {
+        res.status(400).send({
+          msg: "u r not an Admin,Invalid credentialss",
+        });
+      } else {
+        const alreadystoredpassword = Adminexist.password;
+        const ispasswordmatch = await bcrypt.compare(
+          password,
+          alreadystoredpassword
+        )
+     if(ispasswordmatch){
+        res.status(200).send("password matched admin login successful")
+     }
+     else{
+        res.status(400).send("password not matched")
+     }
+    }
+  
+})
+app.post("/login", async function(req,res){
+    const {username,password}=req.body
+    const userexist = await Client.db("FoodToken").collection("users").findOne({username:username})
+    if (!userexist) {
+        res.status(400).send({
+          msg: "Invalid credentialss",
+        });
+      } else {
+        const alreadystoredpassword = userexist.password;
+        const ispasswordmatch = await bcrypt.compare(
+          password,
+          alreadystoredpassword
+        )
+     if(ispasswordmatch){
+        res.status(200).send("password matched login successful")
+     }
+     else{
+        res.status(400).send("password not matched")
+     }
+    }
+  
+})
+
+    app.post("/signup",async function(req,res){
+        const {username,password} = req.body
+      
+       const userexist= await Client.db("FoodToken").collection("users").findOne({username:username})
+      
+        if(userexist){
+            res.status(400).send("Username already exist")
+        }else{
+              const fullhashedpassword = await genhashedpassword(password)
+              // console.log("password: ",fullhashedpassword)
+       const Adduser= await Client.db("FoodToken").collection("users").insertOne({username:username,password:fullhashedpassword})
+       res.status(200).send("Signup successfull")
+            
+        }
+      
+      });
+  
+
 app.get("/cam/:id", async function(req,res){
    const {id}=req.params
 
@@ -175,5 +238,21 @@ app.post("/Admin/AddProduct",async function(req,res){
     const set = await Client.db("B33WD").collection("Product").insertOne(data)
     res.send(set)
 })
+
+
+
+async function genhashedpassword(password) {
+    const rounds = 10;
+    const salt = await bcrypt.genSalt(rounds);
+    // console.log(salt,"this is salt")
+    const hashedpassword = await bcrypt.hash(password, salt);
+    // console.log("hassed",hashedpassword);
+    return hashedpassword;
+  }
+
+
+
+
+
 
 app.listen(PORT, () => console.log(`App started in ${PORT}`));
